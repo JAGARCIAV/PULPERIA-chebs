647b4823dc8cbf800b593891d8cde07f6c0303e3
GARCIA VALLEJOS JOSE ARMANDO <81819461+JAGARCIAV@users.noreply.github.com>
2026-04-02 09:51:21 -0400

    limpieza

--- basededatos_v2_cliente.sql
+++
@@ -1,6 +0,0 @@
--- FASE 2C.0: Soporte relacional en movimientos
-ALTER TABLE movimientos_inventario 
-ADD COLUMN referencia_id INT UNSIGNED NULL AFTER motivo,
-ADD COLUMN referencia_tipo VARCHAR(20) NULL AFTER referencia_id;
-
-CREATE INDEX idx_mov_referencia ON movimientos_inventario (referencia_tipo, referencia_id);
